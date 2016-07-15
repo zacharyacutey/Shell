69d07b70fae4cdbdda805adb5671bbb8ffe1f33b
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-07-15 18:05:09 -0400

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,2 +1,23 @@
-vars = {}
-def parse(arg)
+namespace = {}
+def parse(arg):
+  res = ""
+  while i != len(arg):
+    if arg[i] == '(':
+      res += "shell(["
+    elif arg[i] == ',':
+      res += ","
+    elif arg[i] == '"':
+      res += "\""
+    elif arg[i] == ')':
+      res += "])"
+    else:
+      res += arg[i]
+    i += 1
+  return res
+
+def shell(parenthed):
+  global namespace
+  command = parenthed[0]
+  argv = parenthed[1:]
+  
+  
